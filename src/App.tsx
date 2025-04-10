7d347be7c427b70e361953f2516acef46bd61268
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2025-04-09 21:48:59 -0400

    Chance for team members

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer} from 'react-toastify';
-import LoginPageComponent from '@/app/pages/login-page/LoginPageComponent';
 import TestPageComponent from '@/app/pages/test-page/TestPageComponent';
 import HomePageComponent from '@/app/pages/home-page/HomePageComponent';
 import UnknownPageComponent from '@/app/pages/404-page/UnknownPageComponent';
-import SignupPageComponent from '@/app/pages/signup-page/SignupPageComponent';
+
 import EventDetailPageComponent from '@/app/pages/event-detail-page/EventDetailPageComponent';
 
 
@@ -20,7 +19,6 @@ import ResetPageComponent from '@/app/pages/reset-password-page/ResetPageCompone
 
 
 import ProfilePageComponent from '@/app/pages/profile-page/ProfilePageComponent';
-import CreateEventPageComponent from '@/app/pages/create-event-page/CreateEventPageComponent';
 
 
 
@@ -35,16 +33,15 @@ function App() {
             <Route index element={<HomePageComponent />} />
             <Route path="home" element={<HomePageComponent />} />
             <Route path="test" element={<TestPageComponent />} />
-            <Route path="404" element={<LoginPageComponent />} />
             <Route path="profile" element={<ProfilePageComponent />} />
-            <Route path="create-event" element={<CreateEventPageComponent />} />
+         
             <Route path="event/:id" element={<EventDetailPageComponent />} />
             <Route path="*" element={<UnknownPageComponent />} /> {/* catch-all */}
           </Route>
 
           {/* Standalone routes without the layout/header */}
-          <Route path="/login" element={<LoginPageComponent />} />
-          <Route path="/signup" element={<SignupPageComponent />} />
+         
+         
           <Route path="/reset" element={<ResetPageComponent />} />
         </Routes>
       </Router>
